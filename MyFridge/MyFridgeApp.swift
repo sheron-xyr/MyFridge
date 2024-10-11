c13a8200a4e91eff2df93cad22329f5263befadb
sheron-xyr <1546482874@qq.com>
2024-10-11 18:03:02 +0800

    Initial Commit

---
+++ MyFridge/MyFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFridgeApp.swift
+//  MyFridge
+//
+//  Created by 徐艺榕 on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct MyFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyFridgeTests/MyFridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyFridgeTests.swift
+//  MyFridgeTests
+//
+//  Created by 徐艺榕 on 2024/10/11.
+//
+
+import Testing
+@testable import MyFridge
+
+struct MyFridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
